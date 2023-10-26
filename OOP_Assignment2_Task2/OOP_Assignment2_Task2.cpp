3b27a9774042662ed8a4266dc0fda916aa8a1aa0
Omer Yassir <omerym10@gmail.com>
2023-10-26 13:04:32 +0300

    Add project files.

---
+++ OOP_Assignment2_Task2/OOP_Assignment2_Task2.cpp
@@ -0,0 +1,12 @@
+﻿// OOP_Assignment2_Task2.cpp : Defines the entry point for the application.
+//
+
+#include "OOP_Assignment2_Task2.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
