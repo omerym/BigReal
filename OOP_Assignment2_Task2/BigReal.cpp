01464c6114bb99372031991e8cb6a149dc931ab3
Omar-Salah123 <sd.omar04@gmail.com>
2023-10-31 21:37:28 +0200

    set up beginning for BigReal

---
+++ OOP_Assignment2_Task2/BigReal.cpp
@@ -0,0 +1,16 @@
+#ifndef _BigReal
+#define _BigReal
+
+#include <iostream>
+#include <string>
+#include <deque>
+#include "BCDDigit.cpp"
+#include "PackedBCD.cpp"
+#include "UnsignedBigReal.cpp"
+using namespace std;
+ 
+ class BigReal : UnsignedBigReal{
+    
+    public:
+ };
+#endif //BigReal
